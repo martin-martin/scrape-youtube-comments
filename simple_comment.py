54902917bd3a471a0a39ed05ac601a4d0ce66b73
martin-martin <breuss.martin@gmail.com>
2018-08-22 16:15:04 +0200

    selenium tweets

--- simple_comment.py
+++ simple_comment.py
@@ -7,33 +7,37 @@ from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
 
 
-def youtube_login(email,password):
+def sm_login(email,password):
     # Browser
     driver = webdriver.Chrome('/Users/martin/Documents/codingnomads/nlpython/big_projects/comments/chromedriver')  # changed to Chromium
-    driver.get('https://accounts.google.com/ServiceLogin?hl=en&continue=https%3A%2F%2Fwww.youtube.com%2Fsignin%3Fhl%3Den%26feature%3Dsign_in_button%26app%3Ddesktop%26action_handle_signin%3Dtrue%26next%3D%252F&uilel=3&passive=true&service=youtube#identifier')
+    driver.get('https://twitter.com/login')
     # log in
-    driver.find_element_by_id("identifierId").send_keys(email)
-    driver.find_element_by_id('identifierNext').click()
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "password")))
-    driver.find_element_by_xpath('//*[@id="password"]/div[1]/div/div[1]/input').send_keys(password)
-    driver.find_element_by_id('passwordNext').click()
+    driver.find_element_by_xpath("//input[@placeholder='Phone, email or username']").send_keys(email)
+    driver.find_element_by_xpath("//div[@class='clearfix field']//input[@placeholder='Password']").send_keys(password)
+    driver.find_element_by_xpath("//button[@type='submit']").click()
     return driver
 
-email = os.environ.get('GOOGLE_ACCT')
-password = os.environ.get('GOOGLE_PWD')
-print(email, password)
-driver = youtube_login(email, password)
+# get credentials from open-source-safe environment variables
+email = os.environ.get('TWITTER_ACCT')
+password = os.environ.get('TWITTER_PWD')
+
+
+driver = sm_login(email, password)
 
 # post a comment
-comment = "test"
+comment = "<3"
 
-box = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "box")))
+# find the tweet box
+tweet_box_path = "//div[@id='tweet-box-home-timeline']"
+box = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, tweet_box_path)))
 box.click()
 
-frame = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//iframe[@title="+1"]')))
-driver.switch_to.frame(frame)
+# insert new tweet comment
+driver.execute_script("arguments[0].innerHTML='{}';".format(comment), box)
 
-driver.find_element_by_xpath('//div[@onclick]').click()
+# click the button
+button_path = "//div[@class='home-tweet-box tweet-box component tweet-user']//span[@class='button-text tweeting-text'][contains(text(),'Tweet')]"
+button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, button_path)))
+button.click()
 
-element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//div[@jsname="msEQQc"]/following-sibling::div//div[@g_editable="true"]')))
-driver.execute_script("arguments[0].innerHTML='%s';" % comment, element)
+print("tweeted:", comment)
